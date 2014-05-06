41356193670864e53aeb3b4733a409b4e463be20
Pranjal Pandit <pranjal.pandit@ibibogroup.com>
2014-05-06 17:46:14 +0530

    add memcache example server

---
+++ examples/memcache_get.py
@@ -0,0 +1,18 @@
+# vim:ts=4:sw=4:expandtab
+'''Non blocking memcache get/set.
+'''
+from diesel import Application, Service, send
+from diesel.protocols.memcache import MemcacheClient
+
+def delay_echo_server(addr):
+    # default to 127.0.0.1:11211
+    m = MemcacheClient('localhost')
+    value = m.get('mykey')
+    send("value from memcache key : %s %s"%(str(value), str(non_value)))
+
+
+import pdb 
+pdb.set_trace()
+app = Application()
+app.add_service(Service(delay_echo_server, 8013))
+app.run()
